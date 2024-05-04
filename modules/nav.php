16cd8b488f233a67a1939f3a8d0ae247bb8ba563
Aryan Ranjan <145105301+aryan9099@users.noreply.github.com>
2024-05-04 20:23:36 +0530

    Update nav.php

--- modules/nav.php
+++ modules/nav.php
@@ -15,16 +15,16 @@
 10							<a href="index.php?page=dashboard"><i class="glyphicon glyphicon-home"></i> Dashboard</a>
 11					</li>
 12					<li <?php if ($_GET['page'] == 'take-attendance') echo 'class="active"'; ?>>
-13							<a href="index.php"><i class="glyphicon glyphicon-calendar"></i> Take Attendance</a>
+13							<a href="index.php" target="_blank"><i class="glyphicon glyphicon-calendar"></i> Take Attendance</a>
 14					</li>
 15					<li <?php if ($_GET['page'] == 'studentinfo') echo 'class="active"'; ?>>
-16							<a href="index.php?page=studentinfo"><i class="glyphicon glyphicon-user"></i> Students List</a>
+16							<a href="index.php?page=studentinfo" target="_blank"><i class="glyphicon glyphicon-user"></i> Students List</a>
 17					</li>
 18					<li <?php if ($_GET['page'] == 'reports') echo 'class="active"'; ?>>
 19							<a href="index.php?page=reports"><i class="glyphicon glyphicon-duplicate"></i> Reports</a>
 20					</li>
 21					<li <?php if ($_GET['page'] == 'logout') echo 'class="active"'; ?>>
-22							<a href="index.php?page=logout"><i class="glyphicon glyphicon-off"></i> Logout</a>
+22							<a href="index.php?page=logout" target="_blank"><i class="glyphicon glyphicon-off"></i> Logout</a>
 23					</li>
 24			</ul>
 	</nav>
