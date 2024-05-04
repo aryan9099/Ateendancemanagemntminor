5f0ef8d1144391043ca24282f7a3b11d2065ede2
Aryan Ranjan <145105301+aryan9099@users.noreply.github.com>
2024-05-04 19:35:09 +0530

    Update nav.php

--- modules/nav.php
+++ modules/nav.php
@@ -11,22 +11,22 @@
 						
 							</a>
 					</li>
-					<li>
-							<a href="index.php?page=dashboard"><i class="glyphicon glyphicon-home"></i> Dashboard</a>
-					</li>
-					<li>
-							<a href="index.php"><i class="glyphicon glyphicon-calendar"></i> Take Attendance</a>
-					</li>
-					<li>
-							<a href="index.php?page=studentinfo"><i class="glyphicon glyphicon-user"></i> Students List</a>
-					</li>
-					<li>
-							<a href="index.php?page=reports"><i class="glyphicon glyphicon-duplicate"></i> Reports</a>
-					</li>
-					<li>
-							<a href="index.php?page=logout"><i class="glyphicon glyphicon-off"></i> Logout</a>
-					</li>
-			</ul>
+9					<li <?php if ($_GET['page'] == 'dashboard') echo 'class="active"'; ?>>
+10							<a href="index.php?page=dashboard"><i class="glyphicon glyphicon-home"></i> Dashboard</a>
+11					</li>
+12					<li <?php if ($_GET['page'] == 'take-attendance') echo 'class="active"'; ?>>
+13							<a href="index.php"><i class="glyphicon glyphicon-calendar"></i> Take Attendance</a>
+14					</li>
+15					<li <?php if ($_GET['page'] == 'studentinfo') echo 'class="active"'; ?>>
+16							<a href="index.php?page=studentinfo"><i class="glyphicon glyphicon-user"></i> Students List</a>
+17					</li>
+18					<li <?php if ($_GET['page'] == 'reports') echo 'class="active"'; ?>>
+19							<a href="index.php?page=reports"><i class="glyphicon glyphicon-duplicate"></i> Reports</a>
+20					</li>
+21					<li <?php if ($_GET['page'] == 'logout') echo 'class="active"'; ?>>
+22							<a href="index.php?page=logout"><i class="glyphicon glyphicon-off"></i> Logout</a>
+23					</li>
+24			</ul>
 	</nav>
 <?php else: ?>
 	<nav class="navbar navbar-inverse navbar-fixed-top" id="sidebar-wrapper" role="navigation">
